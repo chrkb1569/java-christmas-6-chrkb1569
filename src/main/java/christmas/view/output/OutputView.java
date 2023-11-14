e52a2484bd46b6312e982d4b6be32724148908dd
minsub <chrkb1569@naver.com>
2023-11-14 20:44:00 +0900

    refactor : OutputView의 필드값을 OutputMessage로 이동시킨다.

--- src/main/java/christmas/view/output/OutputView.java
+++ src/main/java/christmas/view/output/OutputView.java
@@ -6,25 +6,9 @@ import christmas.exception.NotValidInputException;
 
 import java.util.List;
 
+import static christmas.view.output.OutputMessage.*;
+
 public class OutputView {
-    private final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private final String ORDERS = "%n<주문 메뉴>";
-    private final String INITIAL_COST = "%n<할인 전 총주문 금액>";
-    private final String FREEBIE_MESSAGE = "%n<증정 메뉴>";
-    private final String DAY_DISCOUNT_MESSAGE = "크리스마스 디데이 할인: -%,d원";
-    private final String WEEKEND_DISCOUNT_MESSAGE = "평일 할인: -%,d원";
-    private final String HOLIDAY_DISCOUNT_MESSAGE = "주말 할인: -%,d원";
-    private final String SPECIAL_DISCOUNT_MESSAGE = "특별 할인: -%,d원";
-    private final String FREEBIE_DISCOUNT_MESSAGE = "증정 이벤트: -%,d원";
-    private final String FREEBIE = "샴페인 1개";
-    private final String BENEFIT_MESSAGE = "%n<혜택 내역>";
-    private final String NOT_BENEFIT = "없음";
-    private final String ORDER_MESSAGE = "%s %d개";
-    private final String COST_MESSAGE = "%,d원";
-    private final String DISCOUNT_MESSAGE = "-%,d원";
-    private final String TOTAL_DISCOUNT = "%n<총혜택 금액>";
-    private final String TOTAL_RESULT_MESSAGE = "%n<할인 후 예상 결제 금액>";
-    private final String BADGE_RESULT_MESSAGE = "%n<12월 이벤트 배지>";
     private final String BADGE_SANTA = "산타";
     private final String BADGE_TREE = "트리";
     private final String BADGE_STAR = "별";
@@ -36,29 +20,33 @@ public class OutputView {
         System.out.println(START_MESSAGE);
     }
 
+    public void printDateBenefit(int date) {
+        System.out.println(String.format(DATE_BENEFIT_MESSAGE.toString(), date));
+    }
+
     public void printOrderInfo(List<UserInput> inputValues) {
-        System.out.println(String.format(ORDERS));
+        System.out.println(String.format(ORDERED_MENU_MESSAGE.toString()));
 
         for(UserInput inputValue : inputValues) {
-            System.out.println(String.format(ORDER_MESSAGE, inputValue.getMenu(), inputValue.getQuantity()));
+            System.out.println(String.format(ORDERED_ITEM.toString(), inputValue.getMenu(), inputValue.getQuantity()));
         }
     }
 
     public void printInitialCost(Benefit benefit) {
-        System.out.println(String.format(INITIAL_COST));
-        System.out.println(String.format(COST_MESSAGE, benefit.getInitialCost()));
+        System.out.println(String.format(TOTAL_COST_BEFORE_DISCOUNT.toString()));
+        System.out.println(String.format(COST_MESSAGE.toString(), benefit.getInitialCost()));
     }
 
     public void printFreebieItem(Benefit benefit) {
-        System.out.println(String.format(FREEBIE_MESSAGE));
+        System.out.println(String.format(FREEBIE_MESSAGE.toString()));
         System.out.println(getFreebieMessage(benefit.getFreebie()));
     }
 
     public void printBenefits(Benefit benefit) {
-        System.out.println(String.format(BENEFIT_MESSAGE));
+        System.out.println(String.format(BENEFIT_LIST_MESSAGE.toString()));
 
         if(checkResultMessage(benefit)) {
-            System.out.println(NOT_BENEFIT);
+            System.out.println(NOT_BENEFIT_MESSAGE);
             return;
         }
 
@@ -70,17 +58,17 @@ public class OutputView {
     }
 
     public void printTotalDiscount(Benefit benefit) {
-        System.out.println(String.format(TOTAL_DISCOUNT));
-        System.out.println(String.format(DISCOUNT_MESSAGE, benefit.getTotalDiscountValue()));
+        System.out.println(String.format(TOTAL_BENEFIT_MESSAGE.toString()));
+        System.out.println(String.format(DISCOUNTED_COST_MESSAGE.toString(), benefit.getTotalDiscountValue()));
     }
 
     public void printResultCost(Benefit benefit) {
-        System.out.println(String.format(TOTAL_RESULT_MESSAGE));
-        System.out.println(String.format(COST_MESSAGE, benefit.getTotalResult()));
+        System.out.println(String.format(TOTAL_COST_AFTER_DISCOUNT.toString()));
+        System.out.println(String.format(COST_MESSAGE.toString(), benefit.getTotalResult()));
     }
 
     public void printBadgeMessage(Benefit benefit) {
-        System.out.println(String.format(BADGE_RESULT_MESSAGE));
+        System.out.println(String.format(EVENT_BADGE_MESSAGE.toString()));
         System.out.println(getBadgeMessage(benefit));
     }
 
@@ -89,8 +77,8 @@ public class OutputView {
     }
 
     private String getFreebieMessage(boolean freebieValue) {
-        if(freebieValue) return FREEBIE;
-        return NOT_BENEFIT;
+        if(freebieValue) return FREEBIE_ITEM.toString();
+        return NOT_BENEFIT_MESSAGE.toString();
     }
 
     private boolean checkResultMessage(Benefit benefit) {
@@ -107,27 +95,27 @@ public class OutputView {
 
     private void printDayDiscountMessage(Benefit benefit) {
         int dayDiscount = benefit.getDayDiscountValue();
-        if(dayDiscount != 0) System.out.println(String.format(DAY_DISCOUNT_MESSAGE, dayDiscount));
+        if(dayDiscount != 0) System.out.println(String.format(DAY_DISCOUNT_MESSAGE.toString(), dayDiscount));
     }
 
     private void printWeekendDiscountMessage(Benefit benefit) {
         int weekendDiscountValue = benefit.getWeekendDiscountValue();
-        if(weekendDiscountValue != 0) System.out.println(String.format(WEEKEND_DISCOUNT_MESSAGE, weekendDiscountValue));
+        if(weekendDiscountValue != 0) System.out.println(String.format(WEEKEND_DISCOUNT_MESSAGE.toString(), weekendDiscountValue));
     }
 
     private void printHolidayDiscountMessage(Benefit benefit) {
         int holidayDiscountValue = benefit.getHolidayDiscountValue();
-        if(holidayDiscountValue != 0) System.out.println(String.format(HOLIDAY_DISCOUNT_MESSAGE, holidayDiscountValue));
+        if(holidayDiscountValue != 0) System.out.println(String.format(HOLIDAY_DISCOUNT_MESSAGE.toString(), holidayDiscountValue));
     }
 
     private void printSpecialDiscountMessage(Benefit benefit) {
         int specialDiscountValue = benefit.getSpecialDiscountValue();
-        if(specialDiscountValue != 0) System.out.println(String.format(SPECIAL_DISCOUNT_MESSAGE, specialDiscountValue));
+        if(specialDiscountValue != 0) System.out.println(String.format(SPECIAL_DISCOUNT_MESSAGE.toString(), specialDiscountValue));
     }
 
     private void printFreebieMessage(Benefit benefit) {
         boolean freebie = benefit.getFreebie();
-        if(freebie) System.out.println(String.format(FREEBIE_DISCOUNT_MESSAGE, benefit.FREE_BIE_ITEM_PRICE));
+        if(freebie) System.out.println(String.format(FREEBIE_DISCOUNT_MESSAGE.toString(), benefit.FREE_BIE_ITEM_PRICE));
     }
 
     private String getBadgeMessage(Benefit benefit) {
@@ -136,6 +124,6 @@ public class OutputView {
         if(totalDiscountValue >= EXPENSE_SANTA_BADGE) return BADGE_SANTA;
         if(totalDiscountValue >= EXPENSE_TREE_BADGE) return BADGE_TREE;
         if(totalDiscountValue >= EXPENSE_STAR_BADGE) return BADGE_STAR;
-        return NOT_BENEFIT;
+        return NOT_BENEFIT_MESSAGE.toString();
     }
 }
